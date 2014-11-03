f770ea6f25abc33c71321999cda56b671f82abbd
wjiang <wjiang@redhat.com>
2014-11-03 15:55:50 +0800

    init variable with null

--- js/index.js
+++ js/index.js
@@ -72,7 +72,7 @@ function initNagivator()
 {
     makeRequest('architecture/tags.json');
     alert(content);
-    setTimeout(func(){},2000)
+    setTimeout(func(){},100)
     tags = JSON.parse(content);
     makeRequest('architecture/categories.json');
     categories = JSON.parse(content);
