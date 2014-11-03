e6413fa3950539d68f9f0b50b437c1aec2ffc57a
corehello <corehello@gmail.com>
2014-11-03 23:17:46 +0800

    p

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 var httpRequest = null;
+var content = null;
 var tags = null;
 var categories = null;
 var blogs = null;
 
-function makeRequest(url, type_data) 
+function makeRequest(url, type_data, container) 
 {
     if (window.XMLHttpRequest) { // Mozilla, Safari, ...
       httpRequest = new XMLHttpRequest();
@@ -40,6 +41,9 @@ function makeRequest(url, type_data)
                 break;
               case "blogs":
                 renderBlogs(JSON.parse(httpRequest.responseText));
+                break;
+              case "blog":
+                insertContentToContainer(httpRequest.responseText, container);
                 break;
               default:
                 alert("not supported this funciton");
@@ -83,7 +87,7 @@ function init()
 
 function initNagivator()
 {
-    makeRequest('architecture/tags.json',"tags");
+    makeRequest('architecture/tags.json',"tags", content);
 }
 
 
@@ -93,23 +97,61 @@ function renderTags(tags)
     var element = document.getElementById("tags"); 
     for(i=0; i< tags.data.length; i++)
     {
-        var tagconent  = tags.data[i];
+        var tagcontent  = tags.data[i];
         var newtag = document.createElement("div");
         newtag.setAttribute("class", "tag");
-        var nt = newtag.createTextNode(tags.data[i].name + "("+ tags.data[i].count + ")");
+        var nt = newtag.createTextNode(tagcontent.name + "("+ tagcontent.count + ")");
         newtag.appendChild(nt);
+        element.appendChild(newtag);
     }
 }
+
 
 function renderCates(cates)
 {
     
 }
 
-function initContents(blogs)
+function initContents()
 {
-    makeRequest('architecture/blogs.json', 'blogs');
+  makeRequest('architecture/blogs.json', 'blogs', content);
+}
+
+/*
+ *  {
+ *    "name" : "blogs",
+ *    "data" : [
+ *      {
+ *        "url" : "",
+ *        "created_at" : "",
+ *        "tags" : [
+ *          "tags1",
+ *          "tags2"
+ *          ]
+ *      }
+ *
+ *    ]
+ *  }
+ */
+
+function renderBlogers(blogs)
+{
+  var i;
+  var element = document.getElementById("blogs")
+  for(i=0; i<blogs.data.length; i++)
+  {
+    var blogcontent = blogs.data[i];
+    var newblog = document.createElement("div");
+    newblog.setAttribute("class", "blog");
+    var nb = newblog.createElement("div");
+    makeRequest('blogs/'+blogcontent.url, 'blog', nb);
+    newblog.appendChild(nb);
+    element.appendChild(newblog);
+  }
 }
 
 
-
+function insertContentToContainer(content, container)
+{
+  container.innerHTML = content; 
+}
